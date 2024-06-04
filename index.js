b0d4bad4ebe55595cee8fd2410bc475c1fe0cc01
maab-fw <maabfw@proton.me>
2024-06-04 12:39:33 +0600

    jwt and logout api created

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(cookieParser())
 app.use(
     cors({
         origin: ["http://localhost:5173"],
+        credentials: true,
     }),
 )
 
@@ -29,12 +30,29 @@ const client = new MongoClient(uri, {
     },
 })
 
+const cookieOptions = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect()
         const db = client.db("forumFusionDB")
         const usersCollection = db.collection("users")
+
+        // jwt related api
+        app.post("/jwt", async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: "365d" })
+            res.cookie("token", token, cookieOptions).send({ success: true })
+        })
+
+        app.post("/logout", async (req, res) => {
+            res.clearCookie("token", { ...cookieOptions, maxAge: 0 }).send({ success: true })
+        })
 
         // user data insert to the db
         app.post("/users", async (req, res) => {

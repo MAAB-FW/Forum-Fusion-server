1de0a72a3993fd7417ce4b5fde03efde62a9d632
maab-fw <maabfw@proton.me>
2024-06-04 10:10:37 +0600

    mongodb setup connected

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const jwt = require("jsonwebtoken")
 const cookieParser = require("cookie-parser")
 require("dotenv").config()
+const { MongoClient, ServerApiVersion } = require("mongodb")
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -16,6 +17,33 @@ app.use(
         origin: ["http://localhost:5173"],
     }),
 )
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wkufpua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect()
+        
+        
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 })
+        console.log("Pinged your deployment. You successfully connected to MongoDB!")
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close()
+    }
+}
+run().catch(console.dir)
 
 app.get("/", (req, res) => {
     res.send("forum fusion is running...")

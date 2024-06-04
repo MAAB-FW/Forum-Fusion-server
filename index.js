af7bae70c78be2b6e392211dcd243dd19977019f
maab-fw <maabfw@proton.me>
2024-06-05 00:45:51 +0600

    get api created for announcements

--- index.js
+++ index.js
@@ -127,6 +127,12 @@ async function run() {
             res.send(result)
         })
 
+        // get announcements
+        app.get('/announcements',async(req,res)=>{
+            const result = await announcementsCollection.find().toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 })
         console.log("Pinged your deployment. You successfully connected to MongoDB!")

1b372e6d75d3ec755d994b2c0f87b3ba4f6533ee
maab-fw <maabfw@proton.me>
2024-06-04 11:12:56 +0600

    api created for get users data from db

--- index.js
+++ index.js
@@ -48,6 +48,12 @@ async function run() {
             res.send(result)
         })
 
+        // get all users data from db //TODO: verifyAdmin
+        app.get("/users", async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 })
         console.log("Pinged your deployment. You successfully connected to MongoDB!")

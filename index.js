9b21e2e7300f1a7bcbeb022c22ab8f8b6e438fd5
maab-fw <maabfw@proton.me>
2024-06-06 06:49:39 +0600

    delete api created for single post

--- index.js
+++ index.js
@@ -196,6 +196,14 @@ async function run() {
             res.send(result)
         })
 
+        // delete one post from db
+        app.delete("/deletePost/:id", verifyToken, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await postsCollection.deleteOne(query)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 })
         console.log("Pinged your deployment. You successfully connected to MongoDB!")
